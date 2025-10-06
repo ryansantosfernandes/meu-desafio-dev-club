4d91374b8fe1bdb06ea55baa18af0d83f368cff2
ryansantosfernandes <ryan.sfernandes20009@gmail.com>
2025-10-06 10:46:46 -0300

    arrumei o link do 1t

--- xbox-series-S/script.js
+++ xbox-series-S/script.js
@@ -11,7 +11,7 @@ const changeValue = () => {
     } else if (option.value === '1t') {
         linkXbox.classList.add('link-xbox-1t');
         linkXbox.innerHTML = "Compre Aqui O Xbox Series S1TB!";
-        linkXbox.href = "https://www.amazon.com.br/PlayStation%C2%AE5-Slim...";
+        linkXbox.href = "https://www.amazon.com.br/Xbox-1TB-Console-Robot-White/dp/B0D934SHFG/ref=asc_df_B0D934SHFG?mcid=99400a6b8dae33dea05f39db24551b3b&tag=googleshopp00-20&linkCode=df0&hvadid=709884703630&hvpos=&hvnetw=g&hvrand=16516107415037124965&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9197894&hvtargid=pla-2365041323731&language=pt_BR&gad_source=1&th=1";
     }
 
 };
